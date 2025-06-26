4c352662de1a3f7c4a09948bef9a4181a20dfcf1
dev-mahfuj80 <mahfuj@nexstack.sg>
2025-06-26 18:37:02 +0600

    feat: configure SendGrid email provider with default settings

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "juliasedefdjian@strapi.io",
+        defaultReplyTo: "juliasedefdjian@strapi.io",
+        testAddress: "juliasedefdjian@strapi.io",
+      },
+    },
+  },
+});
